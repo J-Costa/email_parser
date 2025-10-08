9da60bdeb03806cc1747eeeecf44f6ba7ea39cbc
Josue Costa <44443163+J-Costa@users.noreply.github.com>
2025-10-08 17:36:59 -0300

    Adjusts on views (#12)
    * Changes style for nav and helper to show active tab
    
    * Adjusts on views and adds show for logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,14 @@
 class LogsController < ApplicationController
+  before_action :set_log, only: [ :show ]
   def index
     @logs = Log.order(created_at: :desc)
+  end
+
+  def show; end
+
+  private
+
+  def set_log
+    @log = Log.find(params[:id])
   end
 end
