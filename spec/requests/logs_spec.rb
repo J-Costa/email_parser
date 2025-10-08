8f4cfcb989e094c98395625dbf40e27b8c4b87ae
Josue Costa <juncosta@gmail.com>
2025-10-08 12:01:25 -0300

    rm unused helper files, rubocop amd fix test

--- spec/requests/logs_spec.rb
+++ spec/requests/logs_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe "Logs", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/logs/index"
+      get logs_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
